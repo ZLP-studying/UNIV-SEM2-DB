3504727cde1a2000249ff9b6d769b87fe9157b31
Nejern <nejern@yandex.ru>
2023-03-17 21:46:12 +0300

    Added rooms structures gen

--- gen_rooms.py
+++
@@ -1,6 +0,0 @@
-import random
-print("insert into structures(object_id, room_type_id, square) \n values")
-for i in range(1,51):
-    j = random.randint(1,8)
-    for k in range(j):
-        print("(" + str(i)+ ",",str(random.randint(1,4))+",",str(random.randint(5,50))+"),")
