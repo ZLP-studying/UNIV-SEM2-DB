0d167caa34f5d6f176a734c8884d83022c5e1431
Nejern <nejern@yandex.ru>
2023-04-29 20:30:44 +0300

    lab_6: completed ex 1-5, ex 6 not tested, Report ex 1-5

---
+++ labs/lab_6/reqests.sql
@@ -0,0 +1,208 @@
+-- 1 -----------------------------------------------------------
+-- Добавить в таблицу «Объекты недвижимости» колонку, которая 
+-- будет хранить стоимость 1 м2. Создать триггер, который будет 
+-- автоматически рассчитывать и записывать стоимость объекта 
+-- недвижимости при добавлении новой записи (значение поля 
+-- «Стоимость» не указывать).
+----------------------------------------------------------------
+ALTER TABLE objects 
+ADD COLUMN cost_per_m2 double precision;
+
+CREATE OR REPLACE FUNCTION lab_6_ex1()
+RETURNS TRIGGER AS $$
+BEGIN
+	NEW.cost = NEW.square * NEW.cost_per_m2;
+	RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE OR REPLACE TRIGGER lab_6_ex1
+BEFORE INSERT OR UPDATE
+ON objects
+FOR EACH ROW
+EXECUTE FUNCTION lab_6_ex1();
+
+INSERT INTO objects
+(square, cost_per_m2) VALUES
+(2, 50);
+SELECT id, cost, square, cost_per_m2
+FROM objects
+ORDER BY id DESC;
+
+-- 2 -----------------------------------------------------------
+-- В таблицу «Риэлторы» добавить колонку «Паспортные данные». 
+-- Создать триггер, который будет проверять корректность 
+-- паспортных данных по маске: ХХХХ УУУУУУ, 
+-- где Х – серия паспорта, У – номер паспорта. 
+-- Между серией и номером паспорта пробел. При несоответствии 
+-- вводимой информации маске, выводить сообщение.
+----------------------------------------------------------------
+ALTER TABLE realtors
+ADD COLUMN passport character varying(11);
+
+CREATE OR REPLACE FUNCTION lab_6_ex_2()
+RETURNS TRIGGER AS $$
+BEGIN
+	IF NEW.passport NOT LIKE '____ ______' THEN
+		RAISE EXCEPTION 'ERROR! Invalid passport input.';
+	END IF;
+	RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE OR REPLACE TRIGGER lab_6_ex_2
+BEFORE INSERT OR UPDATE
+ON realtors
+FOR EACH ROW
+EXECUTE FUNCTION lab_6_ex_2();
+
+INSERT INTO realtors
+(passport) VALUES
+('1253 12122');
+
+-- 3 -----------------------------------------------------------
+-- Создать триггер, который при добавлении нового риэлтора 
+-- формат телефона:79996667788 преобразует в: +7 (999) 666 77 88
+----------------------------------------------------------------
+ALTER TABLE realtors 
+ALTER COLUMN contacts TYPE character varying(24);
+
+CREATE OR REPLACE FUNCTION lab_6_ex_3()
+RETURNS TRIGGER AS $$
+BEGIN
+	NEW.contacts = CONCAT(
+		'+', substr(NEW.contacts, 1, 1),
+		' (', substr(NEW.contacts, 2, 3), ') ',
+		substr(NEW.contacts,5,3), ' ',
+		substr(NEW.contacts,8,2), ' ',
+		substr(NEW.contacts,10,2), ' ');
+	RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE OR REPLACE TRIGGER lab_6_ex_3
+BEFORE INSERT OR UPDATE
+ON realtors
+FOR EACH ROW
+EXECUTE FUNCTION lab_6_ex_3();
+
+INSERT INTO realtors
+(contacts) VALUES
+(79992223311);
+
+SELECT id, contacts
+FROM realtors
+ORDER BY id DESC;
+
+-- 4 -----------------------------------------------------------
+-- Создать триггер, который будет проверять соответствие 
+-- добавляемой оценки определенному диапазону [0,X].
+----------------------------------------------------------------
+CREATE OR REPLACE FUNCTION lab_6_ex_4()
+RETURNS TRIGGER AS $$
+BEGIN
+	IF NEW.rate < 0 OR NEW.rate > 5 THEN
+		RAISE EXCEPTION 'ERROR! Invalid grade input (Must be in range [0, 5]).';
+	END IF;
+	RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE OR REPLACE TRIGGER lab_6_ex_4
+BEFORE INSERT OR UPDATE
+ON rates
+FOR EACH ROW
+EXECUTE FUNCTION lab_6_ex_4();
+
+INSERT INTO rates
+(rate) VALUES
+(6);
+
+-- 5 -----------------------------------------------------------
+-- Создать триггер, который при добавлении новой продажи, 
+-- выводит сообщение, если хотя бы по одному критерию 
+-- данный объект имеет оценку ниже Х.
+----------------------------------------------------------------
+CREATE OR REPLACE FUNCTION lab_6_ex_5()
+RETURNS TRIGGER AS $$
+DECLARE
+lo_parameter_id INTEGER;
+lo_rate FLOAT;
+BEGIN
+	FOR lo_parameter_id IN SELECT DISTINCT parameter_id FROM rates WHERE object_id = NEW.object_id LOOP
+		SELECT rate INTO lo_rate
+		FROM rates
+		WHERE
+		object_id = NEW.object_id
+		AND parameter_id = lo_parameter_id;
+		IF lo_rate < 5 THEN
+			RAISE EXCEPTION 'ERROR! Object rate is below 5 on parameter %.', lo_parameter_id;
+		END IF;
+	END LOOP;
+	RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE OR REPLACE TRIGGER lab_6_ex_5
+BEFORE INSERT
+ON sales
+FOR EACH ROW
+EXECUTE FUNCTION lab_6_ex_5();
+
+SELECT DISTINCT
+objects.id, rates.parameter_id, rates.rate
+FROM sales, rates, objects
+WHERE
+objects.id = 2
+AND sales.object_id = objects.id
+AND rates.object_id = objects.id
+ORDER BY objects.id, rates.parameter_id;
+
+INSERT INTO sales
+(object_id) VALUES
+(2);
+
+-- 6 -----------------------------------------------------------
+-- Создать таблицу, которая следующие сведения: дата, время, 
+-- операция (добавления, обновление, удаление данных),
+-- пользователь (текущий пользовать в СУБД). Добавить триггер, 
+-- который при изменении данных в таблице «Продажи», будет 
+-- фиксировать такое события в таблице-журнале.
+----------------------------------------------------------------
+CREATE TABLE sales_log (
+    id SERIAL PRIMARY KEY,
+    event_date DATE NOT NULL,
+    event_time TIME NOT NULL,
+    operation TEXT NOT NULL,
+    user_name TEXT NOT NULL
+);
+
+CREATE OR REPLACE FUNCTION lab_6_ex6()
+RETURNS TRIGGER AS $$
+BEGIN
+    IF TG_OP = 'DELETE' THEN
+        INSERT INTO sales_log (event_date, event_time, operation, user_name) 
+        VALUES (current_date, current_time, 'DELETE', current_user);
+    ELSIF TG_OP = 'UPDATE' THEN
+        INSERT INTO sales_log (event_date, event_time, operation, user_name) 
+        VALUES (current_date, current_time, 'UPDATE', current_user);
+    ELSIF TG_OP = 'INSERT' THEN
+        INSERT INTO sales_log (event_date, event_time, operation, user_name) 
+        VALUES (current_date, current_time, 'INSERT', current_user);
+    END IF;
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER sales_log_trigger
+AFTER INSERT OR UPDATE OR DELETE
+ON sales
+FOR EACH ROW
+EXECUTE FUNCTION lab_6_ex6();
+
+-- 7 -----------------------------------------------------------
+-- Создать триггер, который при добавлении нового риэлтора будет 
+-- проверять корректность фамилии, имени, отчество (наличие 
+-- только букв, первая буква прописная). При несоответствии, 
+-- выводить сообщение.
+----------------------------------------------------------------
